ba41888eedbf70d4896696aad611b01c2cc6b9e1
prachi.sharma01sep@gmail.com <prachi.sharma01sep@gmail.com>
2019-06-04 17:05:11 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Hello and welcome to Flask</h1>'
+
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:   #if POST
+        name = request.form.get('name')
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if username == 'praachi' and password == 'sharma':
+            return render_template('logout.html', name=name)
+        else:
+            return 'Invalid credentials!'
+
+
+if __name__ == '__main__':
+    app.run(use_reloader = True, debug = True)
+
